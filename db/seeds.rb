55845f82794eb1bb297272f6284b58cc8a3344b4
Shannon Staszak <sestaszak@gmail.com>
2015-05-06 16:28:45 -0500

    fixing seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 new_user = User.create!(first_name: "Jim", last_name: "Joyce", username: "Lebron James", email: "jjoyce1@me.com", password_hash: "asdf")
 
-new_commenter = User.create!(first_name: "Christian")
+new_commenter = User.create!(first_name: "Christian", username: "cmb", password_hash: "moo")
 new_question = Question.create!(title:"How do you ride a bike?", body: "i dont know how.", author_id: new_user.id)
 new_question = Question.create!(title:"Why do bad things happen to good people?", body: "I stubbed my toe and it hit me", author_id: new_user.id)
 
