7957bea374adf698bd5cf8806d3a233898ff0cc3
lcmbl <cbarentine@gmail.com>
2015-05-06 21:35:23 -0500

    Checking in

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,7 +1,7 @@
 class Tag < ActiveRecord::Base
   validates :word, {uniqueness: true}
   validates :word, {presence: true}
-
+# minor change!
   has_many :taggings
   has_many :questions, through: :taggings
 end
