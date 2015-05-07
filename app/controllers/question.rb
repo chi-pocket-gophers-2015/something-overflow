09406d2b47fc82227ca26e7903b3f64aec49ecf2
lcmbl <cbarentine@gmail.com>
2015-05-07 16:18:48 -0500

    fixed voting

--- app/controllers/question.rb
+++ app/controllers/question.rb
@@ -74,18 +74,28 @@ get '/upvote/:question_id/:id' do
   end
 end
 
-get '/downvote/question/:question_id/:answer_id/:id' do
-  current_answer = Question.find_by_id(params[:answer_id])
+get '/downvote/:question_id/:id' do
+  current_question = Question.find_by_id(params[:question_id])
+  if current_question.check_repeat(current_user)
+    current_question.votes.create(value: -1, author_id: params[:id]).save!
+    redirect ("/questions/#{current_question.id}")
+  else
+    redirect ("/questions/#{current_question.id}")
+  end
+end
+
+get '/downvote/:question_id/:answer_id/:id' do
+  current_answer = Answer.find_by_id(params[:answer_id])
   if current_answer.check_repeat(current_user)
-    current_answer.votes.create(value: 1, author_id: params[:id]).save!
+    current_answer.votes.create(value: -1, author_id: params[:id]).save!
     redirect ("/questions/#{params[:question_id]}")
   else
     redirect ("/questions/#{params[:question_id]}")
   end
 end
 
-get '/upvote/question/:question_id/:answer_id/:id' do
-  current_answer = Question.find_by_id(params[:answer_id])
+get '/upvote/:question_id/:answer_id/:id' do
+  current_answer = Answer.find_by_id(params[:answer_id])
   if current_answer.check_repeat(current_user)
     current_answer.votes.create(value: 1, author_id: params[:id]).save!
     redirect ("/questions/#{params[:question_id]}")
